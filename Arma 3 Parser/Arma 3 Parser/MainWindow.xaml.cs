4d67253aea6c74b1ded77833a35ab3e2d7205fec
DeadlyDays <zach@xenophobiaxrt.com>
2017-02-16 22:28:15 -0600

    Update -Organize GUI XAML

--- Arma 3 Parser/Arma 3 Parser/MainWindow.xaml.cs
+++ Arma 3 Parser/Arma 3 Parser/MainWindow.xaml.cs
@@ -24,5 +24,26 @@ namespace Arma_3_Parser
         {
             InitializeComponent();
         }
+
+        private void btnBrowseConfig_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text documents (.txt)|*.txt";
+
+            // Display OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+                txtC.Text = filename;
+            }
+        }
     }
 }
