71288c9de1d984cfefa77a1297b9caac43c1faa2
DeadlyDays <zach@xenophobiaxrt.com>
2017-02-16 21:38:05 -0600

    Update -Classes

---
+++ Arma 3 Parser/Arma 3 Parser/A3Class.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arma_3_Parser
+{
+    [Serializable]
+    class A3Class
+    {
+        String a3ClassName;
+        String fileLocation;
+        String lineLocation;
+        
+        List<A3Class> children;//all of the child classes
+        List<String> inheritanceTree;//list of all the parent elements(things extended + classes nested within)
+        List<String> nestedTree;//just the inheritance tree of classes nested within
+        List<String> extendedTree;//just the inheritance tree of the classes extended
+        List<String> originalCode;//this is the class dec + brackets and all between
+        List<String> content;//This is everything between the brackets
+        List<A3Variable> variables;//All the variables and their values for this class
+
+        A3Class() { }
+
+    }
+}
---
+++ Arma 3 Parser/Arma 3 Parser/A3Variable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arma_3_Parser
+{
+    [Serializable]
+    class A3Variable
+    {
+        String fieldName;
+        String varClassName;//the name of the class this belongs to
+        int linePosition;//the line location of this variable declaration
+
+        List<String> value;//All the contents of the A3 Variable
+        List<String> originalCode;//All the code that declared the field and its value
+        A3Variable() { }
+
+
+    }
+}
