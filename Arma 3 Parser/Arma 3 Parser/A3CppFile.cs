7f06fe893ffddf5084369f4dc81c59e5bddd2b62
DeadlyDays <zach@xenophobiaxrt.com>
2017-02-17 03:46:00 -0600

    Update -added base class parsing logic

--- Arma 3 Parser/Arma 3 Parser/A3CppFile.cs
+++ Arma 3 Parser/Arma 3 Parser/A3CppFile.cs
@@ -32,11 +32,38 @@ namespace Arma_3_Parser
             for (int i = 0; i < originalCodeLineList.Count; i++)//iterates through originalCodeLintList
             {
                 String cursor = originalCodeLineList[i];//value of current index of List
+                
+                if((depth == 0) & (cursor.Contains("class")) && (cursor.Contains(";")))//this is a class that has no contents;
+                {
+                    if (cursor.Contains("class"))//found a new class dec
+                    {
+                        a3c.OriginalCode.Add(cursor);//add originalcode line
+                        int loc = cursor.IndexOf("class");//find where in the line the class keyword starts
+                        loc += 5;//find the location where the actual classname starts
+                        int end = endOfWord(cursor, loc);//the point the classname ends
+                        int length = end - loc;//the length of the classname
+                        a3c.A3ClassName = cursor.Substring(loc, length);//grab the className;
+                        if (cursor.Contains(":"))//does this class extend a base class
+                        {
+                            loc = cursor.IndexOf(":");
+                            loc = startOfNextWord(cursor, loc);//find start of baseClass Name
+                            end = endOfWord(cursor, loc);
+                            length = end - loc;
+                            a3c.ExtendedTree.Add(cursor.Substring(loc, length));//base class name;
+
+                        }
+                    }
+                    A3ClassList.Add(a3c);//store class
+                    a3c = new A3Class();//clear class
+                    continue;
+                }
+
                 //First Check, has depth increased
                 if(cursor.Contains("{"))
                 {
+                    a3c.OriginalCode.Add(cursor);
                     //Need to handle if multiple opens and closes on a single line(god forbid)
-                    if(((Regex.Split(cursor, "{")).Length) > 1)
+                    if (((Regex.Split(cursor, "{")).Length) > 1)
                     {
                         depth += Regex.Split(cursor, "{").Length;
                     }
@@ -44,8 +71,9 @@ namespace Arma_3_Parser
                         depth++;
                 }
                 //check if depth has decreased (can happen on same line)
-                if(cursor.Contains("}"))
+                if(cursor.Contains("}") && (depth > 1))
                 {
+                    a3c.OriginalCode.Add(cursor);
                     //Need to handle if multiple opens and closes on a single line(god forbid)
                     if (((Regex.Split(cursor, "}")).Length) > 1)
                     {
@@ -53,6 +81,14 @@ namespace Arma_3_Parser
                     }
                     else
                         depth--;
+                }
+                else if(cursor.Contains("}"))//if the depth is 1 and closing, that is the end of this class
+                {
+                    a3c.OriginalCode.Add(cursor);
+                    depth--;//decrease depth
+                    A3ClassList.Add(a3c);//store class
+                    a3c = new A3Class();//clear class
+                    continue;
                 }
 
                 //if depth is 0, check for new class dec
@@ -69,9 +105,19 @@ namespace Arma_3_Parser
                         if(cursor.Contains(":"))//does this class extend a base class
                         {
                             loc = cursor.IndexOf(":");
+                            loc = startOfNextWord(cursor, loc);//find start of baseClass Name
+                            end = endOfWord(cursor, loc);
+                            length = end - loc;
+                            a3c.ExtendedTree.Add(cursor.Substring(loc, length));//base class name;
+
                         }
                     }
                 }
+                else
+                {
+                    a3c.OriginalCode.Add(cursor);
+                }
+
 
             }
         }
@@ -82,11 +128,22 @@ namespace Arma_3_Parser
             for(int i = startOfWord; i < line.Length; i++)
             {
                 if(line[i].Equals(' ') || line[i].Equals(':') || line[i].Equals(';') || line[i].Equals('\\') || line[i].Equals('/') || line[i].Equals('{') || line[i].Equals('['))
-                {
                     return i;
-                }
             }
             return end;
+        }
+        public int startOfNextWord(String line, int startLoc)
+        {
+            int result = startLoc;
+            for(int i = startLoc; i < line.Length; i++)
+            {
+                if (line[i].Equals(' ') || line[i].Equals(':') || line[i].Equals(';') || line[i].Equals('\\') || line[i].Equals('/') || line[i].Equals('{') || line[i].Equals('['))
+                    ;
+                else
+                    return i;
+            }
+
+            return result;
         }
 
         public String FilePath
