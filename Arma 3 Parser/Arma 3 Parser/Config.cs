a18fd5922dbf2da8302a6a5cff89ec2abec6a069
DeadlyDays <zach@xenophobiaxrt.com>
2017-02-16 21:51:41 -0600

    Update -Config

--- Arma 3 Parser/Arma 3 Parser/Config.cs
+++ Arma 3 Parser/Arma 3 Parser/Config.cs
@@ -10,6 +10,100 @@ namespace Arma_3_Parser
     class Config
     {
 
-        Config() { }
+        private String pboPath, binPath, cppPath, serialPath, outputPath, unpackPath, convertPath;
+        public Config(){}
+        
+        public String PBOPath
+        {
+            get
+            {
+                if (pboPath == null)
+                    return "";
+                return pboPath;
+            }
+            set
+            {
+                pboPath = value;
+            }
+        }
+        public String BinPath
+        {
+            get
+            {
+                if (binPath == null)
+                    return "";
+                return binPath;
+            }
+            set
+            {
+                binPath = value;
+            }
+        }
+        public String CPPPath
+        {
+            get
+            {
+                if (cppPath == null)
+                    return "";
+                return cppPath;
+            }
+            set
+            {
+                cppPath = value;
+            }
+        }
+        public String SerialPath
+        {
+            get
+            {
+                if (serialPath == null)
+                    return "";
+                return serialPath;
+            }
+            set
+            {
+                serialPath = value;
+            }
+        }
+        public String OutputPath
+        {
+            get
+            {
+                if (outputPath == null)
+                    return "";
+                return outputPath;
+            }
+            set
+            {
+                outputPath = value;
+            }
+        }
+        public String UnpackPath
+        {
+            get
+            {
+                if (unpackPath == null)
+                    return "";
+                return unpackPath;
+            }
+            set
+            {
+                unpackPath = value;
+            }
+        }
+        public String ConvertPath
+        {
+            get
+            {
+                if (convertPath == null)
+                    return "";
+                return convertPath;
+            }
+            set
+            {
+                convertPath = value;
+            }
+        }
+
     }
 }
