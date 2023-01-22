209c723a3a5253505274560d66e0490f6cd6117e
DeadlyDays <deadlydays@gmail.com>
2023-01-22 16:57:09 -0600

    Define Tables

--- Arma 3 Parser/Arma 3 Parser/Parser.cs
+++ Arma 3 Parser/Arma 3 Parser/Parser.cs
@@ -16,7 +16,66 @@ namespace Arma_3_Parser
 
         public Parser()
         {
-            //Empty Constructor
+            //Track Classes
+            DataTable ClassTB = DB.Tables.Add("ClassTB");
+            DataColumn C_pk_ClassUID = ClassTB.Columns.Add("C_pk_ClassUID", typeof(Int32));
+            ClassTB.Columns.Add("C_Name", typeof(String));
+            ClassTB.Columns.Add("C_ParentName", typeof(String));
+            ClassTB.Columns.Add("C_Filename", typeof(String));
+            ClassTB.Columns.Add("C_LineClassDef", typeof(Int32));
+            ClassTB.Columns.Add("C_LineClassStart", typeof(Int32));
+            ClassTB.Columns.Add("C_LineClassEnd", typeof(Int32));
+
+            ClassTB.PrimaryKey = new DataColumn[] { C_pk_ClassUID };
+
+            //Track Variables
+            DataTable VariableTB = DB.Tables.Add("VariableTB");
+            DataColumn V_pk_VariableUID = VariableTB.Columns.Add("V_pk_VariableUID", typeof(Int32));
+            VariableTB.Columns.Add("V_Name", typeof(String));
+            VariableTB.Columns.Add("V_Value", typeof(String));
+
+            VariableTB.PrimaryKey = new DataColumn[] { V_pk_VariableUID };
+
+            //Track Arrays
+            DataTable ArrayTB = DB.Tables.Add("ArrayTB");
+            DataColumn A_pk_ArrayUID = ArrayTB.Columns.Add("A_pk_ArrayUID", typeof(Int32));
+            ArrayTB.Columns.Add("A_Name", typeof(String));
+
+            ArrayTB.PrimaryKey = new DataColumn[] { A_pk_ArrayUID };
+
+            //Lookup for Variables
+            DataTable VariableJoinTB = DB.Tables.Add("VariableJoinTB");
+            DataColumn VJ_pk_VJ_UID = VariableJoinTB.Columns.Add("VJ_pk_VJ_UID", typeof(Int32));
+            DataColumn VJ_fk_ClassUID = VariableJoinTB.Columns.Add("VJ_fk_ClassUID", typeof(Int32));
+            DataColumn VJ_fk_VariableUID = VariableJoinTB.Columns.Add("VJ_fk_VariableUID", typeof(Int32));
+
+            VariableJoinTB.PrimaryKey = new DataColumn[] { VJ_pk_VJ_UID };
+            DB.Relations.Add("ClassVariableJoin",
+                DB.Tables["ClassTB"].Columns["C_pk_ClassUID"],
+                DB.Tables["VariableJoinTB"].Columns["VJ_fk_ClassUID"]);
+            DB.Relations.Add("VariableVariableJoin",
+                DB.Tables["VariableTB"].Columns["V_pk_VariableUID"],
+                DB.Tables["VariableJoinTB"].Columns["VJ_fk_VariableUID"]);
+
+            //Lookup for ArrayVariables
+            DataTable ArrayJoinTB = DB.Tables.Add("ArrayJoinTB");
+            DataColumn AJ_pk_UID = ArrayJoinTB.Columns.Add("AJ_pk_UID", typeof(Int32));
+            DataColumn AJ_fk_ArrayUID = ArrayJoinTB.Columns.Add("AJ_fk_ArrayUID", typeof(Int32));
+            DataColumn AJ_fk_ClassUID = ArrayJoinTB.Columns.Add("AJ_fk_ClassUID", typeof(Int32));
+            DataColumn AJ_fk_VariableUID = ArrayJoinTB.Columns.Add("AJ_fk_VariableUID", typeof(Int32));
+
+            ArrayJoinTB.PrimaryKey = new DataColumn[] { AJ_pk_UID };
+            DB.Relations.Add("ArrayArrayJoin",
+                DB.Tables["ArrayTB"].Columns["A_pk_ArrayUID"],
+                DB.Tables["ArrayJoinTB"].Columns["AJ_fk_ArrayUID"]);
+            DB.Relations.Add("ClassArrayJoin",
+                DB.Tables["ClassTB"].Columns["C_pk_ClassUID"],
+                DB.Tables["ArrayJoinTB"].Columns["AJ_fk_ClassUID"]);
+            DB.Relations.Add("VariableArrayJoin",
+                DB.Tables["VariableTB"].Columns["V_pk_VariableUID"],
+                DB.Tables["ArrayJoinTB"].Columns["AJ_fk_VariableUID"]);
+
+
         }
 
         public void populateFileContentList(String path)
